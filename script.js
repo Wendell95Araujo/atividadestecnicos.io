831b89cc9a648776a946945adbac40ed30f1a69f
Wendell Araujo <95105539+Wendell95Araujo@users.noreply.github.com>
2022-11-11 15:32:13 -0300

    apk 1.0.5

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function alertUpdate() {
 			var answer = confirm ("Este aplicativo foi atualizado para a versão 1.0.5." + "\n" +  "Deseja atualizar agora?")
 			if (answer == true){
 				setCookie('apk-update', 105);
-				window.location="Atividades Técnicos_1_1.0.apk";
+				window.location="Atividades_Tecnicos_1.0.5.apk";
 			} else{
 			setCookie('apk-update', 10);
 			} 
@@ -26,7 +26,7 @@ function alertUpdate() {
 		var answer = confirm ("Este aplicativo foi atualizado para a versão 1.0.5." + "\n" +  "Deseja atualizar agora?")
 		if (answer == true){
 			setCookie('apk-update', 105);
-			window.location="Atividades Técnicos_1_1.0.apk";
+			window.location="Atividades_Tecnicos_1.0.5.apk";
 		} else{
 		setCookie('apk-update', 10);
 		}
