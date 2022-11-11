240fbef76d15d2d996ee2cdb462877d9e0ee1409
Wendell Araujo <95105539+Wendell95Araujo@users.noreply.github.com>
2022-11-11 16:19:49 -0300

    cookie apk-tecnico-update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 function alertUpdate() {
 
-	var cookieUpdate = getCookie('apk-update');
+	var cookieUpdate = getCookie('apk-tecnico-update');
 
 	
 	if (cookieUpdate != '') {
@@ -19,13 +19,13 @@ function alertUpdate() {
 				setCookie('apk-update', 105);
 				window.location="Atividades_Tecnicos_1.0.5.apk";
 			} else{
-			setCookie('apk-update', 10);
+			setCookie('apk-tecnico-update', 10);
 			} 
 		} 
 	} else{
 		var answer = confirm ("Este aplicativo foi atualizado para a versão 1.0.5." + "\n" +  "Deseja atualizar agora?")
 		if (answer == true){
-			setCookie('apk-update', 105);
+			setCookie('apk-tecnico-update', 105);
 			window.location="Atividades_Tecnicos_1.0.5.apk";
 		} else{
 		setCookie('apk-update', 10);
