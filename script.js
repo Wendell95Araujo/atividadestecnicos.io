038fc82083bad611a3c1fc1e74f879ad299c4ac9
Wendell Araujo <95105539+Wendell95Araujo@users.noreply.github.com>
2022-11-30 12:08:55 -0300

    footer

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 	alertUpdate();
 })();
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('footer').style.bottom = "0";
+  } else {
+    document.querySelector('footer').style.bottom = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+	  document.querySelector('footer').style.bottom = "0";
+	 }
+}
+
+
 function alertUpdate() {
 
 	var cookieUpdate = getCookie('apk-tecnico-update');
